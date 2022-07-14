7bd8e06113e3afeb9f3c290b04286066c467229f
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-07-14 12:14:01 +0900

    ✨ [3]Health CheckのResponseを設計してみよう

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,13 +1,15 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
 )
 
 // A HealthzHandler implements health check endpoint.
-type HealthzHandler struct{}
+type HealthzHandler struct {
+}
 
 // NewHealthzHandler returns HealthzHandler based http.Handler.
 func NewHealthzHandler() *HealthzHandler {
@@ -16,5 +18,9 @@ func NewHealthzHandler() *HealthzHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+	res := &model.HealthzResponse{
+		Message: "OK",
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s", res.Message)
 }
