22c69689a5c8bf98407c367212b64c85b5d15f29
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-07-14 15:47:45 +0900

    [8]TODO を作成する API の Service を実装してみよう

--- service/todo.go
+++ service/todo.go
@@ -25,8 +25,24 @@ func (s *TODOService) CreateTODO(ctx context.Context, subject, description strin
 		insert  = `INSERT INTO todos(subject, description) VALUES(?, ?)`
 		confirm = `SELECT subject, description, created_at, updated_at FROM todos WHERE id = ?`
 	)
+	result, err := s.db.ExecContext(ctx, insert, subject, description)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	id, err := result.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	var todo model.TODO
+	err = s.db.QueryRowContext(ctx, confirm, id).Scan(&todo.Subject, &todo.Description, &todo.CreatedAt, &todo.UpdatedAt)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &todo, nil
 }
 
 // ReadTODO reads TODOs on DB.
