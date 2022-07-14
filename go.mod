3b943dfb784fe319746201b0be309e905e3d6dfd
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-07-14 11:25:24 +0900

    🎉 [02]HTTP サーバーを起動してみよう

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/TechBowl-japan/go-stations
+
+go 1.16
+
+require (
+	github.com/jstemmer/go-junit-report v0.9.1
+	github.com/mattn/go-sqlite3 v1.14.7
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/TechBowl-japan/go-stations/db"
+	"github.com/TechBowl-japan/go-stations/handler/router"
+)
+
+func main() {
+	err := realMain()
+	if err != nil {
+		log.Fatalln("main: failed to exit successfully, err =", err)
+	}
+}
+
+func realMain() error {
+	// config values
+	const (
+		defaultPort   = ":8080"
+		defaultDBPath = ".sqlite3/todo.db"
+	)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	dbPath := os.Getenv("DB_PATH")
+	if dbPath == "" {
+		dbPath = defaultDBPath
+	}
+
+	// set time zone
+	var err error
+	time.Local, err = time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		return err
+	}
+
+	// set up sqlite3
+	todoDB, err := db.NewDB(dbPath)
+	if err != nil {
+		return err
+	}
+	defer todoDB.Close()
+
+	// set http handlers
+	mux := router.NewRouter(todoDB)
+
+	// TODO: ここから実装を行う
+	log.Fatal(http.ListenAndServe(port, mux))
+	return nil
+}
